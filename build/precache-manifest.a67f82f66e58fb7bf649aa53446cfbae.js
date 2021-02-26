a5808070271ac730a22b0c2222f648514fb22dbe
Zorba Jobs Wong <it@zorbawong.com>
2021-02-26 17:40:55 +0900

    First Deploy.

---
+++ build/precache-manifest.a67f82f66e58fb7bf649aa53446cfbae.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7810c0cf84ae660dfab28cd4885853e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d9f343b47e9142ba107",
+    "url": "/static/css/main.809cc550.chunk.css"
+  },
+  {
+    "revision": "8fc88cfa0a6d816f43e0",
+    "url": "/static/js/2.4bc6df6d.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/static/js/2.4bc6df6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d9f343b47e9142ba107",
+    "url": "/static/js/main.b7322b4a.chunk.js"
+  },
+  {
+    "revision": "0b7eb9d469061bce3505",
+    "url": "/static/js/runtime-main.140093ed.js"
+  },
+  {
+    "revision": "5bf453820387cee50a4b6310de9b30f3",
+    "url": "/static/media/background_search_desktop.5bf45382.jpg"
+  },
+  {
+    "revision": "28b9a9206952160265fac07ff7b72b67",
+    "url": "/static/media/background_search_mobile.28b9a920.jpg"
+  }
+]);
