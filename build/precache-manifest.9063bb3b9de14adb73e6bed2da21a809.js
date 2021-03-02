3e357d7b280e849fca9298a057f4b1e56798cc3d
Zorba Jobs Wong <it@zorbawong.com>
2021-03-02 16:35:05 +0900

    v1.2.0 update - to AWS

---
+++ build/precache-manifest.9063bb3b9de14adb73e6bed2da21a809.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "913f3876e8e098a2f09c4e836ba48da8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "435b9e5cf4a069a175c6",
+    "url": "/static/css/main.722b8894.chunk.css"
+  },
+  {
+    "revision": "3a271069a099059bbd15",
+    "url": "/static/js/2.5444d103.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/static/js/2.5444d103.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "435b9e5cf4a069a175c6",
+    "url": "/static/js/main.4fb0c564.chunk.js"
+  },
+  {
+    "revision": "1e3f8a847511a90e7e07",
+    "url": "/static/js/runtime-main.dfd7c9ee.js"
+  },
+  {
+    "revision": "5bf453820387cee50a4b6310de9b30f3",
+    "url": "/static/media/background_search_desktop.5bf45382.jpg"
+  },
+  {
+    "revision": "28b9a9206952160265fac07ff7b72b67",
+    "url": "/static/media/background_search_mobile.28b9a920.jpg"
+  }
+]);
